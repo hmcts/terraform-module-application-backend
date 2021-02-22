44550d9dcc2eb4fb18f066d918cb965db9b7352c
Matt Slater <matt.slater@hmcts.net>
2021-02-22 13:54:22 +0000

    DTSPO-658 - remove provider

--- provider.tf
+++ provider.tf
@@ -1,15 +1,17 @@
-// provider "azurerm" {
-//  features {}
-//  alias = "data"
+# terraform {
+#   required_version = "0.14.4"
 
-//} 
-terraform {
-  required_version = "0.14.4"
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "2.41.0"
+#     }
+#   }
+# }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.41.0"
-    }
-  }
-}
+
+# // provider "azurerm" {
+# //  features {}
+# //  alias = "data"
+
+# //} 
