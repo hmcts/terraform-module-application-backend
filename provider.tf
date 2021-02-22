8805e27532cac77717c4d3d0392bd407c2b84e7a
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-22 11:43:14 +0000

    DTSPO-658 Ammend provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,13 @@
 //  alias = "data"
 
 //} 
+terraform {
+  required_version = "0.14.4"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
+}
