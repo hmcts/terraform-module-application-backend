9ada4f21b49315d1ceb07fab87dd101b1b0085f9
Matt Slater <matt.slater@hmcts.net>
2021-02-22 14:36:06 +0000

    DTSPO-658 - add provider to data source

--- config.tf
+++ config.tf
@@ -38,6 +38,7 @@ data "azurerm_subnet" "app_gw" {
   name                 = "aks-appgw"
   resource_group_name  = local.vnet_rg
   virtual_network_name = local.vnet_name
+  provider = azurerm.data
 }
 
 resource "azurerm_public_ip" "app_gw" {
