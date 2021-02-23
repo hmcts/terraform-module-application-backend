6c9eb6d045106f3137530e277835bf2bf774ce5f
Matt Slater <matt.slater@hmcts.net>
2021-02-23 14:08:07 +0000

    DTSPO-658 - remove locals

--- config.tf
+++ config.tf
@@ -1,38 +1,36 @@
-locals {
-  vnet_rg   = "aks-infra-${var.env}-rg"
-  vnet_name = "core-${var.env}-vnet"
+# locals {
 
-  criticality = {
-    sbox     = "Low"
-    aat      = "High"
-    prod     = "High"
-    ithc     = "Medium"
-    ldata    = "Medium"
-    perftest = "Medium"
-    demo     = "Medium"
-  }
+#   # criticality = {
+#   #   sbox     = "Low"
+#   #   aat      = "High"
+#   #   prod     = "High"
+#   #   ithc     = "Medium"
+#   #   ldata    = "Medium"
+#   #   perftest = "Medium"
+#   #   demo     = "Medium"
+#   # }
 
-  env_display_names = {
-    sbox     = "Sandbox"
-    aat      = "Staging"
-    prod     = "Production"
-    ithc     = "ITHC"
-    ldata    = "LDATA"
-    perftest = "Test"
-    demo     = "Demo"
-  }
+#   # env_display_names = {
+#   #   sbox     = "Sandbox"
+#   #   aat      = "Staging"
+#   #   prod     = "Production"
+#   #   ithc     = "ITHC"
+#   #   ldata    = "LDATA"
+#   #   perftest = "Test"
+#   #   demo     = "Demo"
+#   # }
 
-  tags = {
-    "managedBy"          = "Platform Engineering"
-    "solutionOwner"      = "CFT"
-    "activityName"       = "AKS"
-    "dataClassification" = "Internal"
-    "automation"         = ""
-    "costCentre"         = "10245117" // until we get a better one, this is the generic cft contingency one
-    "environment"        = local.env_display_names[var.env]
-    "criticality"        = local.criticality[var.env]
-  }
-}
+#   # tags = {
+#   #   "managedBy"          = "Platform Engineering"
+#   #   "solutionOwner"      = "CFT"
+#   #   "activityName"       = "AKS"
+#   #   "dataClassification" = "Internal"
+#   #   "automation"         = ""
+#   #   "costCentre"         = "10245117" // until we get a better one, this is the generic cft contingency one
+#   #   "environment"        = local.env_display_names[var.env]
+#   #   "criticality"        = local.criticality[var.env]
+#   # }
+# }
 
 data "azurerm_subnet" "app_gw" {
   name                 = "aks-appgw"
