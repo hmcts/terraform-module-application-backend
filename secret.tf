c73d6e5e92569383ec6324580720bc0eea831960
Darren Bromwich <d.bromwich@kainos.com>
2021-03-04 12:33:12 +0000

    update to main and removed provider

--- secret.tf
+++ secret.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault" "main" {
   name                = var.vault_name
-  resource_group_name = "core-infra-${var.subscription}-rg"
+  resource_group_name = var.key_vault_resource_group
 }
 
 resource "azurerm_key_vault_secret" "test" {
