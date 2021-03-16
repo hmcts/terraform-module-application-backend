9fb785ef347e3de6a84f91feea1c352d831b51cc
mokainos <72137340+mokainos@users.noreply.github.com>
2021-03-16 14:09:29 +0000

    Modified

--- main.tf
+++ main.tf
@@ -56,13 +56,13 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "probe" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name          = "${app.product}-${app.component}"
-      path          = lookup(app, "health_path_override", "/health/liveness")
-      host_name_include_env            = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}-${var.env}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
-      host_name_exclude_env            = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
-      ssl_host_name = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.ssl_host_name_suffix])
-      ssl_enabled   = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
-      exclude_env_in_app_name          = lookup(local.gateways[count.index].gateway_configuration, "exclude_env_in_app_name", false)
+      name                    = "${app.product}-${app.component}"
+      path                    = lookup(app, "health_path_override", "/health/liveness")
+      host_name_include_env   = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}-${var.env}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
+      host_name_exclude_env   = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
+      ssl_host_name           = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.ssl_host_name_suffix])
+      ssl_enabled             = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
+      exclude_env_in_app_name = lookup(local.gateways[count.index].gateway_configuration, "exclude_env_in_app_name", false)
     }]
 
     content {
@@ -100,13 +100,13 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "http_listener" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name                 = "${app.product}-${app.component}"
-      host_name_include_env            = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}-${var.env}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
-      host_name_exclude_env            = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
-      ssl_host_name        = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.ssl_host_name_suffix])
-      ssl_enabled          = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
-      ssl_certificate_name = local.gateways[count.index].gateway_configuration.certificate_name
-      exclude_env_in_app_name          = lookup(local.gateways[count.index].gateway_configuration, "exclude_env_in_app_name", false)
+      name                    = "${app.product}-${app.component}"
+      host_name_include_env   = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}-${var.env}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
+      host_name_exclude_env   = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.host_name_suffix])
+      ssl_host_name           = join(".", [lookup(app, "host_name_prefix", "${app.product}-${app.component}"), local.gateways[count.index].gateway_configuration.ssl_host_name_suffix])
+      ssl_enabled             = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
+      ssl_certificate_name    = local.gateways[count.index].gateway_configuration.certificate_name
+      exclude_env_in_app_name = lookup(local.gateways[count.index].gateway_configuration, "exclude_env_in_app_name", false)
     }]
 
     content {
