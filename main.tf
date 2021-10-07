6f285b568a9c4c58eca7717d5e34fdaa06119ec0
Zoe Cope <zoe.cope@hmcts.net>
2021-10-07 12:13:44 +0100

    add local

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_application_gateway" "ag" {
   resource_group_name = var.vnet_rg
   location            = var.location
   tags                = var.common_tags
-  zones               = var.availability_zones ? list("1", "2", "3") : null
+  zones               = local.zones
 
   count = length(local.gateways)
 
