ddb48742eb5042f8d481aae076bf975356bed566
Zoe Cope <zoe.cope@hmcts.net>
2021-10-07 11:37:51 +0100

    local

--- local.tf
+++ local.tf
@@ -1,3 +1,5 @@
 locals {
-  gateways = yamldecode(data.local_file.configuration.content).gateways
+  gateways                        = yamldecode(data.local_file.configuration.content).gateways
+  regions_with_availability_zones = ["UK South", "West Europe", "eastus", "North Europe"]
+  zones                           = contains(local.regions_with_availability_zones, var.location) ? list("1", "2", "3") : null
 }
--- variables.tf
+++ variables.tf
@@ -53,6 +53,6 @@ variable "log_analytics_workspace_id" {}
 
 variable "availability_zones" {
   description = "List of availability Zones"
-  type        = list
+  type        = list(any)
   default     = []
 }
