f7c0847c1653c0081028c45cfedbcd8db5fbcf47
chrisorisawayi <chris.orisawayi@hmcts.net>
2022-03-02 08:36:59 +0000

    updated pip naming condition

--- publicip.tf
+++ publicip.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_public_ip" "app_gw" {
   count               = length(var.private_ip_address)
-  name                = (var.create_new_agw == true) ? local.new_pip_name : local.pip_name
+  name                = (var.create_new_agw == true) ? (element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "aks-appgateway-${var.env}-pip" : "aks-appgateway-${var.env}-pip-${count.index}") : (element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "aks-appgw-${var.env}-pip" : "aks-appgw-${var.env}-pip-${count.index}")
   location            = var.location
   resource_group_name = var.vnet_rg
   sku                 = "Standard"
