d6112f8e52e7b6aa216cc977b5ee92093e85aa7f
chrisorisawayi <chris.orisawayi@hmcts.net>
2022-03-03 14:10:25 +0000

    moved condition to locals

--- publicip.tf
+++ publicip.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_public_ip" "app_gw" {
   count               = length(var.private_ip_address)
-  name                = (var.resource_prefix != null) ? (element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "${var.resource_prefix}-aks-appgw-${var.env}-pip" : "${var.resource_prefix}-aks-appgw-${var.env}-pip-${count.index}") : (element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "aks-appgw-${var.env}-pip" : "aks-appgw-${var.env}-pip-${count.index}")
+  name                = element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "${local.resource_prefix}aks-appgw-${var.env}-pip" : "${local.resource_prefix}aks-appgw-${var.env}-pip-${count.index}"
   location            = var.location
   resource_group_name = var.vnet_rg
   sku                 = "Standard"
