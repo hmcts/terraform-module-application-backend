289ed11ffff1f81abc303f1c4b5b0ce6e9069377
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-12-07 11:15:49 +0000

    alter priority for redirect rule

--- main.tf
+++ main.tf
@@ -183,7 +183,7 @@ resource "azurerm_application_gateway" "ag" {
   dynamic "request_routing_rule" {
     for_each = [for i, app in local.gateways[count.index].app_configuration : {
       name = "${app.product}-${app.component}-redirect"
-      priority = ((i + 1) * 10)
+      priority = (((i + 1) * 10) + 1)
       }
       if lookup(app, "http_to_https_redirect", false) == true
     ]
